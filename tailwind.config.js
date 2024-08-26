747c607adbaeeb8a01b735d0efc181b7d98abe31
Lucas Pereyra <lucasezequielpereyra0@gmail.com>
2024-08-26 08:57:02 -0300

    [add] standard modals

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+import plugin from 'tailwindcss/plugin'
+
+export const content = ['./src/**/*.{html,js,ts,jsx,tsx}']
+
+export const theme = {
+  extend: {
+    animation: {
+      customLoading: 'customLoading 1.2s ease-in-out infinite both'
+    },
+    keyframes: {
+      customLoading: {
+        '0%, 20%, 80%, 100%': {
+          opacity: '1',
+          backgroundColor: 'rgba(234,235,236,255)'
+        },
+        '50%': {
+          opacity: '1',
+          transform: 'scale(1.3)',
+          backgroundColor: '#9A5FE8'
+        }
+      }
+    }
+  }
 }
 
+export const plugins = [
+  plugin(({ matchUtilities, theme }) => {
+    matchUtilities(
+      {
+        'animation-delay': (value) => {
+          return {
+            'animation-delay': value
+          }
+        }
+      },
+      {
+        values: theme('transitionDelay')
+      }
+    )
+  })
+]
