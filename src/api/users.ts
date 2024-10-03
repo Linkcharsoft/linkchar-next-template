7aac7d9ae82be8840623c62bbbf5652989156d92
Lucas Pereyra <lucasezequielpereyra0@gmail.com>
2024-10-03 11:37:26 -0300

    [update] back urls

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,95 +1,18 @@
 import { customFetch } from './customFetch'
 import type { User } from '@/types/users'
 
+// user endpoints
 export const getMyUser = async (token: string) => {
   return await customFetch<User>({
-    path: '/api/users/me/',
+    path: '/api/auth/user/',
     method: 'GET',
     token
   })
 }
 
-// export const login = async (body: { email: string; password: string }) => {
-//   return await customFetch<LoginResponse>({
-//     path: '/api/auth/login/',
-//     method: 'POST',
-//     body
-//   })
-// }
-
-export const logout = async (token: string) => {
-  return await customFetch<any>({
-    path: '/api/auth/logout/',
-    method: 'POST',
-    token
-  })
-}
-
-export const signup = async (body: { email: string; password1: string, password2: string }) => {
-  return await customFetch<{
-    detail: string
-  }>({
-    path: '/api/auth/register/',
-    method: 'POST',
-    body
-  })
-}
-
-export const passwordRecoveryChange = async (body: {
-  request_type: 'change' | 'reset'
-  email: string
-}) => {
-  return await customFetch<any>({  // type this according to the need 
-    path: '/api/users/password-recovery/',
-    method: 'POST',
-    body
-  })
-}
-
-export const checkPasswordToken = async (body: {  token: string, email: string }) => {
-  return await customFetch({
-    path: '/api/users/password-recovery/check-token/',
-    method: 'POST',
-    body
-  })
-}
-
-export const passwordConfirm  = async (body: {
-  token: string;
-  email: string;
-  password: string;
-}) => {
-  return await customFetch({
-    path: '/api/users/password-recovery/confirm/',
-    method: 'POST',
-    body
-  })
-}
-
-export const emailConfirmation  = async (body: {
-  key: string;
-}) => {
-  return await customFetch({
-    path: '/api/auth/registration/account-email-verification-sent/',
-    method: 'POST',
-    body
-  })
-}
-
-export const resendEmailConfirmation  = async (body: {
-  email: string;
-}) => {
-  return await customFetch({
-    path: '/api/auth/registration/resend-email/',
-    method: 'POST',
-    body
-  })
-}
-
-
 export const updateUserProfile = async (token: string, body: any) => {
   return await customFetch<any>({  // type this according to the need 
-    path: '/api/users/me/',
+    path: '/api/auth/user/',
     method: 'PATCH',
     token,
     body
@@ -101,9 +24,71 @@ export const completeRegistration  = async (token: string, body: {
   last_name: string
 }) => {
   return await customFetch<any>({ // type this according to the need 
-    path: '/api/users/me/complete-register/',
+    path: '/api/auth/user/complete-register/',
     method: 'PATCH',
     token,
+    body
+  })
+}
+
+// auth endpoints
+export const signup = async (body: { email: string; password1: string, password2: string }) => {
+  return await customFetch<{
+    detail: string
+  }>({
+    path: '/api/auth/registration/',
+    method: 'POST',
+    body
+  })
+}
+
+export const passwordRecoveryChange = async (body: {
+  request_type: 'change' | 'reset'
+  email: string
+}) => {
+  return await customFetch<any>({  // type this according to the need 
+    path: '/api/auth/password/recovery/',
+    method: 'POST',
+    body
+  })
+}
+
+export const checkPasswordToken = async (body: {  token: string, email: string }) => {
+  return await customFetch({
+    path: '/api/auth/password/recovery/check-token/',
+    method: 'POST',
+    body
+  })
+}
+
+export const passwordConfirm  = async (body: {
+  token: string;
+  email: string;
+  password: string;
+}) => {
+  return await customFetch({
+    path: '/api/auth/password/recovery/confirm/',
+    method: 'POST',
+    body
+  })
+}
+
+export const emailConfirmation  = async (body: {
+  key: string;
+}) => {
+  return await customFetch({
+    path: 'api/auth/registration/verify-email/',
+    method: 'POST',
+    body
+  })
+}
+
+export const resendEmailConfirmation  = async (body: {
+  email: string;
+}) => {
+  return await customFetch({
+    path: 'api/auth/registration/resend-email/',
+    method: 'POST',
     body
   })
 }
