96f9b7d1124291397114575468daec22f70d9b72
Lucas Pereyra <lucasezequielpereyra0@gmail.com>
2024-10-04 16:19:48 -0300

    [config] remove console log for production

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: false,
+  compiler: {
+    removeConsole: isProd,
+  },
   env: {
     API_URL: process.env.API_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
