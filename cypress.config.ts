905c02c2d29d8fc52f58237f04a14cdd99d33ba8
lukway-dev <lukway.dev@gmail.com>
2024-11-03 22:08:18 -0300

    [Update] Cypress Config

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,19 +9,18 @@ export default defineConfig({
   screenshotOnRunFailure: false,
   e2e: {
     baseUrl: 'http://localhost:3000',
-    viewportWidth: 1536,
-    viewportHeight: 678,
-    defaultCommandTimeout: 20000,
-    pageLoadTimeout: 60000,    
-
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    defaultCommandTimeout: 15000,
+    requestTimeout: 10000,
+    pageLoadTimeout: 60000,
+    testIsolation: false,
+    chromeWebSecurity: false,
+    waitForAnimations: true,
+    experimentalStudio: true, // Enable Cypress Studio for recording
+    experimentalWebKitSupport: true, // Enable WebKit support
     env: {
       API_URL: process.env.API_URL
     },
-
-    // setupNodeEvents(on, config) {
-    //   // Implement node event listeners here, if needed
-    // },
-    experimentalStudio: true // Enable Cypress Studio for recording
   }
 })
-
