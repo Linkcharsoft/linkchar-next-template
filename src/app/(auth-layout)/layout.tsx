b439d0a6760074d9b353bd59311c54b0e7c50f1d
lukway-dev <lukway.dev@gmail.com>
2025-01-18 23:45:59 -0300

    [Update] AuthLayout styles

--- src/app/(auth-layout)/layout.tsx
+++ src/app/(auth-layout)/layout.tsx
@@ -1,24 +1,32 @@
-'use client'
 import Image from 'next/image'
 import LoadingModal from '@/components/modals/LoadingModal'
 import ToastMessages from '@/components/modals/ToastMessages'
 import Logo from '@/assets/images/logo.svg'
 
+
 interface Props {
   children: React.ReactNode
 }
+
 
 const AuthLayout = ({ children }: Props) => {
   return (
     <main className="flex h-screen w-screen overflow-hidden">
       <section className="relative flex w-[45%] items-center justify-center bg-white">
-        {children}
+        { children }
         <LoadingModal />
         <ToastMessages />
       </section>
 
       <section className="h-full w-[55%] bg-black flex justify-center items-center">
-        <Image src={Logo} width={256} height={256} alt="Logo" className="AuthLayout__Logo" priority />
+        <Image
+          src={Logo}
+          width={256}
+          height={256}
+          alt="Logo"
+          className="AuthLayout__Logo"
+          priority
+        />
       </section>
     </main>
   )
--- src/app/(test-layout)/success-login/page.tsx
+++ src/app/(test-layout)/success-login/page.tsx
@@ -21,7 +21,7 @@ const Page = () => {
   const handleSignIn = () => {
     signIn('credentials', { redirect: false })
   }
-  
+
   return (
     <div className="flex flex-col gap-4 m-4">
       <div className="flex flex-col gap-2">
