c185865331d3ed432cbeed2dd896612be3d60bf2
lukway-dev <lukway.dev@gmail.com>
2025-05-01 20:12:45 -0300

    [Update] tsconfig.json

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -7,10 +7,10 @@ import * as Sentry from '@sentry/nextjs'
 if(process.env.IS_PRODUCTION === 'true') {
   Sentry.init({
     dsn: '', // complete with your sentry dsn
-  
+
     // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
     tracesSampleRate: 1,
-  
+
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false
   })
