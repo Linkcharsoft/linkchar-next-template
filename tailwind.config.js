9dcef86398ec6e12ca550691efc1bd6254307e3b
lukway-dev <lukway.dev@gmail.com>
2025-08-11 16:26:59 -0300

    [ ADD ] surface tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ export const content = ['./src/**/*.{html,js,ts,jsx,tsx}']
 
 export const theme = {
   extend: {
+    colors: {
+      'surface-50': '#FAFAFA',
+      'surface-100': '#F5F5F5',
+      'surface-200': '#EEEEEE',
+      'surface-300': '#E0E0E0',
+      'surface-400': '#BDBDBD',
+      'surface-500': '#9E9E9E',
+      'surface-600': '#757575',
+      'surface-700': '#616161',
+      'surface-800': '#424242',
+      'surface-900': '#212121',
+    },
     animation: {
       customLoading: 'customLoading 1.2s ease-in-out infinite both'
     },
