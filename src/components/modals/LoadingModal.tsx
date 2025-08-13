4163181ba084c69aa14fa22e98b20b1fb29c5ea3
lukway-dev <lukway.dev@gmail.com>
2025-08-13 16:16:05 -0300

    [ ADD ] Loader component

--- src/components/modals/LoadingModal.tsx
+++ src/components/modals/LoadingModal.tsx
@@ -2,6 +2,7 @@
 import { AnimatePresence, motion } from 'framer-motion'
 import { memo } from 'react'
 import { useAppStore } from '@/stores/appStore'
+import Loader from '../Loader'
 
 const LoadingModal = () => {
   const { loadingModal } = useAppStore()
@@ -17,21 +18,7 @@ const LoadingModal = () => {
         >
           <div className="mx-8 flex max-w-[700px] flex-col items-center gap-4">
             <div className='relative'>
-
-              <div className="relative h-12 w-12 [&>span]:absolute [&>span]:inset-0 [&>span]:inline-flex [&>span]:h-full [&>span]:w-full [&>span]:before:h-2 [&>span]:before:w-2 [&>span]:before:animate-customLoading [&>span]:before:rounded-full [&>span]:before:bg-indigo-600">
-                <span className=""></span>
-                <span className="rotate-[30deg] before:!animation-delay-[-1.1s]"></span>
-                <span className="rotate-[60deg] before:!animation-delay-[-1s]"></span>
-                <span className="rotate-[90deg] before:!animation-delay-[-0.9s]"></span>
-                <span className="rotate-[120deg] before:!animation-delay-[-0.8s]"></span>
-                <span className="rotate-[150deg] before:!animation-delay-[-0.7s]"></span>
-                <span className="rotate-[180deg] before:!animation-delay-[-0.6s]"></span>
-                <span className="rotate-[210deg] before:!animation-delay-[-0.5s]"></span>
-                <span className="rotate-[240deg] before:!animation-delay-[-0.4s]"></span>
-                <span className="rotate-[270deg] before:!animation-delay-[-0.3s]"></span>
-                <span className="rotate-[300deg] before:!animation-delay-[-0.2s]"></span>
-                <span className="rotate-[330deg] before:!animation-delay-[-0.1s]"></span>
-              </div>
+              <Loader/>
             </div>
 
             {loadingModal.title && (
