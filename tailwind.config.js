8f1144e68ba5d6c4a37d39dd4f98a1b97413fd74
lukway-dev <lukway.dev@gmail.com>
2025-08-13 22:51:29 -0300

    [ REMOVE ] Unused tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import plugin from 'tailwindcss/plugin'
-
 export const content = ['./src/**/*.{html,js,ts,jsx,tsx}']
 
 export const theme = {
@@ -16,39 +14,6 @@ export const theme = {
       'surface-700': '#616161',
       'surface-800': '#424242',
       'surface-900': '#212121',
-    },
-    animation: {
-      customLoading: 'customLoading 1.2s ease-in-out infinite both'
-    },
-    keyframes: {
-      customLoading: {
-        '0%, 20%, 80%, 100%': {
-          opacity: '1',
-          backgroundColor: 'rgba(234,235,236,255)'
-        },
-        '50%': {
-          opacity: '1',
-          transform: 'scale(1.3)',
-          backgroundColor: '#9A5FE8'
-        }
-      }
     }
   }
 }
-
-export const plugins = [
-  plugin(({ matchUtilities, theme }) => {
-    matchUtilities(
-      {
-        'animation-delay': (value) => {
-          return {
-            'animation-delay': value
-          }
-        }
-      },
-      {
-        values: theme('transitionDelay')
-      }
-    )
-  })
-]
