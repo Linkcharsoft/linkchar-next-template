c24c25958bd0161d46642c2e87973d3a14f75478
lukway-dev <lukway.dev@gmail.com>
2025-08-17 20:43:54 -0300

    [ FIX ] ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,12 +4,12 @@ import js from '@eslint/js'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import _import from 'eslint-plugin-import'
-import prettier from 'eslint-plugin-prettier'
 import react from 'eslint-plugin-react'
-import reackHooks from 'eslint-plugin-react-hooks'
+import reactHooks from 'eslint-plugin-react-hooks'
 import cypressPlugin from 'eslint-plugin-cypress'
 import next from '@next/eslint-plugin-next'
 import globals from 'globals'
+import stylistic from '@stylistic/eslint-plugin'
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
@@ -17,24 +17,26 @@ const compat = new FlatCompat({
   allConfig: js.configs.all
 })
 
+
+
 const config = [...fixupConfigRules(compat.extends(
   'eslint:recommended',
+  'next/core-web-vitals',
+  'next/typescript',
+  'plugin:@typescript-eslint/recommended',
   'plugin:import/recommended',
   'plugin:import/typescript',
-  'plugin:react/recommended',
   'plugin:react/jsx-runtime',
-  'plugin:@typescript-eslint/recommended',
-  'next/typescript',
-  'next/core-web-vitals'
+  'plugin:react/recommended'
 )), {
   plugins: {
-    react: fixupPluginRules(react),
-    "react-hooks": fixupPluginRules(reackHooks),
-    next: fixupPluginRules(next),
-    import: fixupPluginRules(_import),
-    prettier,
     '@typescript-eslint': fixupPluginRules(typescriptEslint),
-    cypress: cypressPlugin
+    'react-hooks': fixupPluginRules(reactHooks),
+    cypress: cypressPlugin,
+    import: fixupPluginRules(_import),
+    next: fixupPluginRules(next),
+    react: fixupPluginRules(react),
+    stylistic,
   },
 
   languageOptions: {
@@ -63,13 +65,17 @@ const config = [...fixupConfigRules(compat.extends(
   },
 
   rules: {
-    indent: ['error', 2, {
-      SwitchCase: 1,
-    }],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    'no-undef': 'error',
+    // @stylistic
+    'stylistic/indent': ['error', 2, { SwitchCase: 1 }],
+    'stylistic/array-bracket-spacing': ['warn', 'never'],
+    'stylistic/object-curly-spacing': ['warn', 'always'],
+    'stylistic/quotes': ['error', 'single'],
+    'stylistic/semi': ['error', 'never'],
+    'stylistic/space-before-function-paren': ['warn', 'always'],
+    'stylistic/space-infix-ops': 'warn',
 
+
+    // Import order
     'import/order': ['error', {
       groups: [
         'builtin',
@@ -87,11 +93,15 @@ const config = [...fixupConfigRules(compat.extends(
       },
     }],
 
+    // React / TS
     'react/prop-types': 0,
     'react-hooks/exhaustive-deps': 'off',
+    'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-namespace': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
+
+    // Cypress
     ...cypressPlugin.configs.recommended.rules,
     'cypress/unsafe-to-chain-command': 'off'
   },
@@ -103,9 +113,7 @@ const config = [...fixupConfigRules(compat.extends(
     '.vscode',
     '.husky',
     '.next',
-    'next-env.d.ts',
     'next.config.js',
-    'next-env.d.ts',
     'next-env.d.ts',
     'eslint.config.mjs',
   ]
