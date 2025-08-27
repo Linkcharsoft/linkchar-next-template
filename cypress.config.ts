ab0350ff2f73ec2be9a862b06f9f93b3d51041ee
lukway-dev <lukway.dev@gmail.com>
2025-08-27 19:05:11 -0300

    [ ADD ] MailSlurp

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,16 +11,19 @@ export default defineConfig({
     baseUrl: 'http://localhost:3000',
     viewportWidth: 1920,
     viewportHeight: 1080,
+
     defaultCommandTimeout: 15000,
     requestTimeout: 10000,
     pageLoadTimeout: 60000,
+
     testIsolation: false,
-    chromeWebSecurity: false,
+    // chromeWebSecurity: false,
     waitForAnimations: true,
     experimentalStudio: true, // Enable Cypress Studio for recording
     experimentalWebKitSupport: true, // Enable WebKit support
     env: {
-      API_URL: process.env.API_URL
+      API_URL: process.env.API_URL,
+      MAILSLURP_API_KEY: process.env.MAILSLURP_API_KEY
     },
   }
 })
