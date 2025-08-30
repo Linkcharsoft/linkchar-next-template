5d4490045f101f21abb3dd8b7995a7b488965b9b
lukway-dev <lukway.dev@gmail.com>
2025-08-30 17:50:44 -0300

    [ ADD ] Password recovery screen test

---
+++ cypress/e2e/auth/screens/PasswordRecovery.cy.ts
@@ -0,0 +1,45 @@
+import { AUTH_COOKIE_NAME , AUTH_INPUT_ERRORS } from '../../../../src/constants/auth'
+import { checkInputError } from '../../../support/helpers'
+
+describe('Password Recovery', () => {
+  before(() => {
+    cy.visit('/password-recovery')
+
+    cy.getCookie(AUTH_COOKIE_NAME).should('not.exist')
+  })
+
+  beforeEach(() => {
+    cy.get('input[name="email"]').as('email-input').clear()
+    cy.get('button[type="submit"]').as('submit-button')
+  })
+
+  it('Errors ❌: Required', () => {
+    cy.get('@submit-button').click()
+
+    checkInputError({
+      alias: '@email-input',
+      message: AUTH_INPUT_ERRORS.required
+    })
+  })
+
+  it('Errors ❌: Invalid email', () => {
+    cy.get('@email-input').type('test')
+
+    cy.get('@submit-button').click()
+
+    checkInputError({
+      alias: '@email-input',
+      message: AUTH_INPUT_ERRORS['invalid-email']
+    })
+  })
+
+  it('Navigation 🔗: Login', () => {
+    const baseURL = Cypress.config().baseUrl
+
+    cy.get('a[href="/login"]').click()
+
+    cy.url().should('equal', `${baseURL}/login`)
+
+    cy.visit('/password-recovery')
+  })
+})
