282f415b65d407482a9f1c2d8d54794efba7f182
lukway-dev <lukway.dev@gmail.com>
2026-01-05 14:30:47 -0300

    [ UPDATE ] ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,130 +1,148 @@
-import { fixupConfigRules, fixupPluginRules } from '@eslint/compat'
-import { FlatCompat } from '@eslint/eslintrc'
 import js from '@eslint/js'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
-import _import from 'eslint-plugin-import'
+import importPlugin from 'eslint-plugin-import'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import cypressPlugin from 'eslint-plugin-cypress'
-import next from '@next/eslint-plugin-next'
+import nextPlugin from '@next/eslint-plugin-next'
 import globals from 'globals'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import stylistic from '@stylistic/eslint-plugin'
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-})
-
-const config = [...fixupConfigRules(compat.extends(
-  'eslint:recommended',
-  'next/core-web-vitals',
-  'next/typescript',
-  'plugin:@typescript-eslint/recommended',
-  'plugin:import/recommended',
-  'plugin:import/typescript',
-  'plugin:react/jsx-runtime',
-  'plugin:react/recommended'
-)), {
-  plugins: {
-    '@typescript-eslint': fixupPluginRules(typescriptEslint),
-    'react-hooks': fixupPluginRules(reactHooks),
-    cypress: cypressPlugin,
-    import: fixupPluginRules(_import),
-    next: fixupPluginRules(next),
-    react: fixupPluginRules(react),
-    stylistic,
+const ESLintConfig = [
+  // --- Ignores ---
+  {
+    ignores: ['node_modules/', 'dist/', 'build/', '.next/'],
   },
-
-  languageOptions: {
-    globals: {
-      ...globals.browser,
-      ...globals.node
-    },
-
-    parser: tsParser,
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-  },
-
-  settings: {
-    'import/resolver': {
-      typescript: {
-        project: './tsconfig.json',
-      },
-    },
-  },
-
-  rules: {
-    // @stylistic
-    'stylistic/indent': ['error', 2, { SwitchCase: 1 }],
-    'stylistic/array-bracket-spacing': ['warn', 'never'],
-    'stylistic/object-curly-spacing': ['warn', 'always'],
-    'stylistic/quotes': ['error', 'single'],
-    'stylistic/semi': ['error', 'never'],
-    'stylistic/space-before-function-paren': ['warn', 'always'],
-    'stylistic/space-infix-ops': 'warn',
-
-
-    // Import order
-    'import/order': ['error', {
-      groups: [
-        'builtin',
-        'external',
-        'internal',
-        ['parent', 'sibling'],
-        'index',
-        'object',
-        'type',
-      ],
-
-      pathGroups: [
-        {
-          pattern: '**/*.+(css|scss|sass)',
-          group: 'external',
-          position: 'before',
+  // --- Base Recommended JS Rules ---
+  js.configs.recommended,
+  // --- Main Project Rules ---
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
         }
-      ],
-
-      warnOnUnassignedImports: true,
-
-      alphabetize: {
-        order: 'asc',
-        caseInsensitive: true,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021
       }
-    }],
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      'jsx-a11y': jsxA11y,
+      'react-hooks': reactHooks,
+      import: importPlugin,
+      '@next/next': nextPlugin,
+      react,
+      stylistic
+    },
+    settings: {
+      react: { version: 'detect' },
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json'
+        }
+      }
+    },
+    rules: {
+      // @stylistic
+      'stylistic/array-bracket-spacing': ['warn', 'never'],
+      'stylistic/indent': ['error', 2, { SwitchCase: 1 }],
+      'stylistic/no-multi-spaces': 'error',
+      'stylistic/no-trailing-spaces': 'error',
+      'stylistic/object-curly-spacing': ['error', 'always'],
+      'stylistic/quotes': ['error', 'single'],
+      'stylistic/semi': ['error', 'never'],
+      'stylistic/space-before-function-paren': ['warn', 'always'],
+      'stylistic/space-infix-ops': 'warn',
+      'stylistic/type-annotation-spacing': ['error', { after: true }],
 
-    // React / TS
-    'react/prop-types': 0,
-    'react-hooks/exhaustive-deps': 'off',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-namespace': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
+      // Import order
+      'import/order': ['error', {
+        'groups': [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'index',
+          'object',
+          'type'
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        },
+        'newlines-between': 'always',
+        warnOnUnassignedImports: true,
+        pathGroups: [
+          {
+            pattern: '**/*.+(css|scss|sass)',
+            group: 'external',
+            position: 'before'
+          }
+        ]
+      }],
 
-    // Cypress
-    ...cypressPlugin.configs.recommended.rules,
-    'cypress/unsafe-to-chain-command': 'off'
+      // React & Web (A11y)
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...jsxA11y.configs.recommended.rules,
+      'react/jsx-filename-extension': [1, { 'extensions': ['.jsx', '.tsx'] }],
+      // 'react/jsx-uses-react': 'off',
+      'react/no-unstable-nested-components': 'error',
+      'react/prop-types': 'off',
+      // 'react/react-in-jsx-scope': 'off',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+
+      // Next
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+
+      // TS
+      ...typescriptEslint.configs.recommended.rules,
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      // '@typescript-eslint/no-unused-vars': ['warn', {
+      //   argsIgnorePattern: '^_',
+      //   varsIgnorePattern: '^_'
+      // }],
+    },
   },
+  // --- Cypress Config ---
+  {
+    files: ['cypress/**/*.{ts,tsx}', '**/*.cy.{ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...cypressPlugin.configs.recommended.languageOptions.globals
+      }
+    },
+    plugins: { cypress: cypressPlugin },
+    rules: {
+      ...cypressPlugin.configs.recommended.rules,
+      'cypress/unsafe-to-chain-command': 'off'
+    }
+  },
+  // // --- Config Files ---
+  // {
+  //   files: ['*.config.js', '*.config.ts', 'next.config.js', 'cypress.config.ts'],
+  //   languageOptions: {
+  //     globals: {
+  //       ...globals.node
+  //     }
+  //   }
+  // }
+]
 
-  ignores: [
-    'node_modules',
-    'dist',
-    'build',
-    '.vscode',
-    '.husky',
-    '.next',
-    'next.config.js',
-    'next-env.d.ts',
-    'eslint.config.mjs',
-  ]
-}]
-
-export default config
+export default ESLintConfig
