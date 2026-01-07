4cec5ff97cfdc124ffed39e90fca2be29b3606c7
lukway-dev <lukway.dev@gmail.com>
2026-01-07 15:31:53 -0300

    [ ADD ] Next.js config improvements

---
+++ next.config.ts
@@ -0,0 +1,52 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  reactCompiler: true,
+  reactStrictMode: true,
+  typedRoutes: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+
+  // --- Images config example ---
+  // images: {
+  //   minimumCacheTTL: 3600,
+  //   formats: ['image/avif', 'image/webp'],
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: new URL(process.env.MEDIA_URL).host,
+  //       port: '',
+  //       pathname: '/**',
+  //     },
+  //     {
+  //       protocol: 'https',
+  //       hostname: new URL(process.env.STRAPI_URL).host,
+  //       port: '',
+  //       pathname: '/**',
+  //     },
+  //     {
+  //       protocol: 'https',
+  //       hostname: new URL(process.env.STRAPI_MEDIA_URL).host,
+  //       port: '',
+  //       pathname: '/**',
+  //     }
+  //   ]
+  // }
+
+  // --- ⚠️ Do not use in production ⚠️ ---
+  // experimental: {
+  //   optimizePackageImports: ['primereact', 'primeicons', 'three', 'framer-motion'],
+  //   turbopackFileSystemCacheForDev: true,
+  //   cssChunking: true,
+  //   inlineCss: true,
+  //   webVitalsAttribution: ['CLS', 'FCP', 'FID', 'INP', 'LCP', 'TTFB']
+  // }
+}
+
+export default nextConfig
