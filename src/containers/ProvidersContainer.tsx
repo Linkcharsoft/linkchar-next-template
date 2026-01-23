224300a757dbf9a8fb5e56adfc36a25e0d592b57
lukway-dev <lukway.dev@gmail.com>
2026-01-23 11:59:41 -0300

    [ ADD ] Support to locale "es" on PrimeReact Calendar component

--- src/containers/ProvidersContainer.tsx
+++ src/containers/ProvidersContainer.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Clarity from '@microsoft/clarity'
 import { domAnimation, LazyMotion } from 'framer-motion'
-import { PrimeReactProvider } from 'primereact/api'
+import { addLocale, PrimeReactProvider } from 'primereact/api'
 import Tailwind from 'primereact/passthrough/tailwind'
 import { useEffect } from 'react'
 import useUserStore from '@/stores/userStore'
@@ -28,6 +28,18 @@ const ProvidersContainer = ({ children }: Props) => {
     if(process.env.NEXT_PUBLIC_CLARITY_ID) Clarity.init(process.env.NEXT_PUBLIC_CLARITY_ID)
   }, [])
 
+  useEffect(() => {
+    addLocale('es', {
+      firstDayOfWeek: 1,
+      dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+      dayNamesShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+      dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
+      monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+      monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+      today: 'Hoy',
+      clear: 'Limpiar'
+    })
+  }, [])
 
   return (
     <PrimeReactProvider value={{ pt: Tailwind }}>
