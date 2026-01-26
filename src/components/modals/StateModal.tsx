90c2eb6356a35eaacb4bc4259eadee685c4914c4
lukway-dev <lukway.dev@gmail.com>
2026-01-26 15:37:53 -0300

    [ UPDATE ] StateModal managed by modalStore

--- src/components/modals/StateModal.tsx
+++ src/components/modals/StateModal.tsx
@@ -1,68 +1,65 @@
 'use client'
-import { Button } from 'primereact/button'
 import { Dialog } from 'primereact/dialog'
 import useModalStore from '@/stores/modalStore'
+import CustomButton from '../CustomButton'
 
-/* DOCS: https://primereact.org/dialog/ */
+const TYPE_ICON = {
+  success: 'pi pi-check-circle text-green-500',
+  info: 'pi pi-info-circle text-blue-500',
+  warning: 'pi pi-exclamation-triangle text-orange-500',
+  error: 'pi pi-times-circle text-red-500'
+}
 
 export default function StateModal () {
-  const { modalState, hideModalState } = useModalStore()
-
-  /* THIS IS A EXAMPLE COLORS AND ICONS */
-
-  const TYPE_ICON = {
-    success: 'pi pi-check-circle text-green-500',
-    info: 'pi pi-info-circle text-blue-500',
-    warning: 'pi pi-exclamation-triangle text-yellow-500',
-    error: 'pi pi-times-circle text-red-500'
-  }
-
-  const SERVERITY_BUTTON = {
-    success: 'p-button-success',
-    info: 'p-button-info',
-    warning: 'p-button-warning',
-    error: 'p-button-danger'
-  }
+  const { modals: { stateModal }, closeModal } = useModalStore()
 
   return (
-    <div className="card justify-content-center flex">
-      <Dialog
-        header="Header"
-        visible={modalState.show}
-        focusOnShow={false}
-        draggable={false}
-        headerClassName="text-center"
-        style={{ width: '350px' }}
-        onHide={() => {
-          hideModalState()
-        }}
-        pt={{
-          content: {
-            className: 'py-4 pb-5'
-          },
-          header: {
-            className: 'py-3'
-          },
-          headerTitle: {
-            className: 'text-lg font-bold ml-10'
-          }
-        }}
-      >
-        <div className="flex flex-col items-center gap-4">
-          <i className={TYPE_ICON[modalState.type]} style={{ fontSize: '2rem' }} />
-          <p>{modalState.content}</p>
-          <Button
-            label="OK"
-            className={SERVERITY_BUTTON[modalState.type]}
-            pt={{
-              root: {
-                className: 'w-1/2 h-12'
-              }
-            }}
-            onClick={() => {}}
-          />
+    <Dialog
+      header={stateModal.title}
+      visible={stateModal.show}
+      focusOnShow={false}
+      draggable={false}
+      onHide={() => closeModal('stateModal')}
+      pt={{
+        root: {
+          className: 'w-full max-w-[420px]'
+        },
+        content: {
+          className: 'p-4'
+        },
+        header: {
+          className: 'px-4 py-3 text-center'
+        },
+        headerTitle: {
+          className: 'text-bold-18 ml-8'
+        }
+      }}
+    >
+      <div className="flex flex-col items-center gap-6">
+        <i className={`${TYPE_ICON[stateModal.type]} text-32`}/>
+
+        <div className="flex flex-col items-center gap-2 text-center">
+          {stateModal.subtitle && (
+            <p className='text-semibold-16'>{stateModal.subtitle}</p>
+          )}
+          <p className='text-14'>{stateModal.content}</p>
         </div>
-      </Dialog>
-    </div>
+
+        <CustomButton
+          variant={stateModal.type}
+          pt={{
+            root: {
+              className: 'w-1/2 h-11'
+            }
+          }}
+          onClick={() => {
+            if(stateModal.button?.action) stateModal.button.action()
+            closeModal('stateModal')
+          }}
+        >
+          { stateModal.button?.label ?? 'OK' }
+        </CustomButton>
+      </div>
+    </Dialog>
   )
 }
