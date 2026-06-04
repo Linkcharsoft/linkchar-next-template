747007be57c632b9f2fc062a7068159195efab55
lukway-dev <lukway.dev@gmail.com>
2026-06-04 10:48:20 -0300

    [ CHORE ] Remove dead Sentry webpack block (no-op under Turbopack)

--- next.config.ts
+++ next.config.ts
@@ -116,19 +116,9 @@ export default withSentryConfig(nextConfig, {
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  tunnelRoute: '/monitoring',
+  tunnelRoute: '/monitoring'
 
-  webpack: {
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-
-    // Tree-shaking options for reducing bundle size
-    treeshake: {
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      removeDebugLogging: true
-    }
-  }
+  // NOTE: a `webpack: { treeshake, automaticVercelMonitors }` block used to live here.
+  // It was removed: `next build` runs on Turbopack (Next 16 default) and Sentry's
+  // `webpack` options have no effect under Turbopack, so it was dead config.
 })
